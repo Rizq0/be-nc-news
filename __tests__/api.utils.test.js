b49b85de3cef7ae7befe8ac430d605e94f740046
Joe <joe_ethan2@hotmail.com>
2024-08-28 12:14:02 +0100

    feat: adding test suite to checkExists function

---
+++ __tests__/api.utils.test.js
@@ -0,0 +1,11 @@
+const { checkExists } = require("../api/utility-functions/api-utils");
+
+describe("API Utility Function Testing", () => {
+  describe("checkExists Function Testing", () => {
+    test("When given a correct table name, column name and value that exists in that table, return a confirmation", () => {
+      return checkExists("articles", "article_id", 1).then((data) => {
+        expect(data).toBe("Exists within the database");
+      });
+    });
+  });
+});
